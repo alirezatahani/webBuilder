71ef2a3fc8381c268035df3369add98eb9eb6d44
Behzad <saeedibehzad@gmail.com>
2022-07-24 11:32:18 +0430

    import dynamically templates

--- pages/gridLayout/index.tsx
+++ pages/gridLayout/index.tsx
@@ -1,8 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { Blank1 } from "@templates/plain/blank1";
-import { Blank2 } from "@templates/plain/blank2";
-import { Blank3 } from "@templates/plain/blank3";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
 import { renderToString } from "react-dom/server";
 import {
   ServerStyleSheet,
@@ -10,67 +7,103 @@ import {
   ThemeProvider,
 } from "styled-components";
 import { theme } from "@global/Global";
+import dynamic from "next/dynamic";
 
 const EditTemplate = () => {
-  const BLANK_DESIGNS: any = {
-    "1": <Blank1 />,
-    "2": <Blank2 />,
-    "3": <Blank3 />,
-  };
   const [allElements, setAllElements] = useState({ data: [{}] });
   const router: any = useRouter();
-  const templateId = router.query.template.split("/")[1];
-  const component = BLANK_DESIGNS[templateId];
-  const sheet = new ServerStyleSheet();
-  const html = renderToString(
-    <StyleSheetManager sheet={sheet.instance}>
-      <ThemeProvider theme={theme}>{component}</ThemeProvider>
-    </StyleSheetManager>
+
+  useEffect(() => {
+    if (router.query.template) {
+      const myTemplate = router.query.template
+        .split("/")
+        .toString()
+        .replace(",", "")
+        .replace("b", "B");
+      const Templates = dynamic(() =>
+        import("@templates/index").then((Templates: any) => {
+          console.log(Templates);
+          return Templates[myTemplate];
+        })
+      );
+      console.log(Templates);
+      const sheet = new ServerStyleSheet();
+      const html = renderToString(
+        <StyleSheetManager sheet={sheet.instance}>
+          <ThemeProvider theme={theme}>
+            <Templates />
+          </ThemeProvider>
+        </StyleSheetManager>
+      );
+      const style = sheet.getStyleTags();
+      console.log(html);
+
+      // elemntsArray.map((item, index) => {
+      //   //console.log(index,item);
+      //   const section = {
+      //     i: index,
+      //     text: item,
+      //     w: 2,
+      //     h: 1,
+      //     x: 12,
+      //     y: 0,
+      //   };
+      //   //console.log(section);
+
+      //   // setAllElements((preState) => ({
+      //   //   data: [...preState.data, { item }],
+      //   // }));
+      // });
+    }
+  }, [router]);
+
+  return (
+    <>
+      this is grid layout page
+    </>
   );
-  console.log(html);
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     const width = window.innerWidth;
-  //     const height = window.innerHeight;
-  //     // window.onmouseover = (e) => {
-  //     //   console.log(e.target);
-  //     // };
-  //     const documentElements: any =
-  //       window.document.body.querySelectorAll("Section");
-  //     const documentElementsArray = [...documentElements];
-
-  //     const getPosition = (element: any) => {
-  //       var rect = element.getBoundingClientRect();
-  //       return {
-  //         x: rect.left,
-  //         y: rect.top,
-  //       };
-  //     };
-  //     documentElementsArray.map((element, index) => {
-  //       //get position of element
-  //       const { x, y } = getPosition(element);
-  //       const elementX = Math.round((12 * x) / width);
-  //       //get width of element
-  //       const elementWidth = Math.round((12 * element.offsetWidth) / width);
-  //       const elementheight = Math.round((12 * element.offsetHeight) / height);
-  //       console.log("from key");
-  //       const section = {
-  //         i: index,
-  //         text: element.innerHTML,
-  //         w: elementWidth,
-  //         h: elementheight,
-  //         x: elementX,
-  //         y: 0,
-  //       };
-  //       setAllElements((preState) => ({
-  //         data: [...preState.data, { section }],
-  //       }));
-  //     });
-  //     console.log(allElements);
-  //   }
-  // }, []);
-
-  return <>this is grid layout page</>;
 };
 
 export default EditTemplate;
+//console.log(allElements);
+// useEffect(() => {
+//   if (typeof window !== "undefined") {
+//     const width = window.innerWidth;
+//     const height = window.innerHeight;
+//     // window.onmouseover = (e) => {
+//     //   console.log(e.target);
+//     // };
+//     const documentElements: any =
+//       window.document.body.querySelectorAll("Section");
+//     const documentElementsArray = [...documentElements];
+
+//     const getPosition = (element: any) => {
+//       var rect = element.getBoundingClientRect();
+//       return {
+//         x: rect.left,
+//         y: rect.top,
+//       };
+//     };
+//     documentElementsArray.map((element, index) => {
+//       //get position of element
+//       const { x, y } = getPosition(element);
+//       const elementX = Math.round((12 * x) / width);
+//       //get width of element
+//       const elementWidth = Math.round((12 * element.offsetWidth) / width);
+//       const elementheight = Math.round((12 * element.offsetHeight) / height);
+//       console.log("from key");
+//       const section = {
+//         i: index,
+//         text: element.innerHTML,
+//         w: elementWidth,
+//         h: elementheight,
+//         x: elementX,
+//         y: 0,
+//       };
+//       setAllElements((preState) => ({
+//         data: [...preState.data, { section }],
+//       }));
+//     });
+//     console.log(allElements);
+//   }
+// }, []);
---
+++ templates/index.ts
@@ -0,0 +1,4 @@
+import {Blank1} from "./plain/blank1"
+import {Blank3} from "./plain/blank3"
+import { Blank2} from "./plain/blank2"
+export {Blank1,Blank3,Blank2}
