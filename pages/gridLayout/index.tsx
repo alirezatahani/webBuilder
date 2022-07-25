8d4c450b5d3c453b13bc869ea63e3004d6cc3d82
tahanamdar30 <tahanamdar30@gmail.com>
2022-07-25 12:53:43 +0430

    refactor dynamic import

--- pages/gridLayout/index.tsx
+++ pages/gridLayout/index.tsx
@@ -1,8 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { Blank1 } from "@templates/plain/blank1";
-import { Blank2 } from "@templates/plain/blank2";
-import { Blank3 } from "@templates/plain/blank3";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
 import { renderToString } from "react-dom/server";
 import {
   ServerStyleSheet,
@@ -11,85 +8,56 @@ import {
 } from "styled-components";
 import { theme } from "@global/Global";
 import dynamic from "next/dynamic";
-
+//refactor
 const EditTemplate = () => {
-  const BLANK_DESIGNS: any = {
-    "1": <Blank1 />,
-    "2": <Blank2 />,
-    "3": <Blank3 />,
-  };
-
-  const [sectionArray, setSectionArray] = useState([] as any);
   const [allElements, setAllElements] = useState({ data: [{}] });
   const router: any = useRouter();
-  
 
   useEffect(() => {
     if (router.query.template) {
-      const templateId = router.query.template.split("/")[1];
-      const myUrl = "@templates/";
-      const templates = dynamic<{}>(() =>
-        import(myUrl).then((template) => template)
+      const myTemplate = router.query.template
+        .split("/")
+        .toString()
+        .replace(",", "")
+        .replace("b", "B");
+      const Templates = dynamic(() =>
+        import("@templates/index").then((Templates: any) => {
+          console.log(Templates);
+          return Templates[myTemplate];
+        })
       );
-      const component = BLANK_DESIGNS[templateId];
+      console.log(Templates);
       const sheet = new ServerStyleSheet();
       const html = renderToString(
         <StyleSheetManager sheet={sheet.instance}>
-          <ThemeProvider theme={theme}>{component}</ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <Templates />
+          </ThemeProvider>
         </StyleSheetManager>
       );
+      const style = sheet.getStyleTags();
+      console.log(html);
 
-      const regex = /<section\s+[\s\S]+?<\/section>/g;
-      setSectionArray(html.match(regex));
- 
+      // elemntsArray.map((item, index) => {
+      //   //console.log(index,item);
+      //   const section = {
+      //     i: index,
+      //     text: item,
+      //     w: 2,
+      //     h: 1,
+      //     x: 12,
+      //     y: 0,
+      //   };
+      //   //console.log(section);
+
+      //   // setAllElements((preState) => ({
+      //   //   data: [...preState.data, { item }],
+      //   // }));
+      // });
     }
   }, [router]);
-
-  console.log(sectionArray);
 
   return <>this is grid layout page</>;
 };
 
 export default EditTemplate;
-
-// useEffect(() => {
-//   if (typeof window !== "undefined") {
-//     const width = window.innerWidth;
-//     const height = window.innerHeight;
-//     // window.onmouseover = (e) => {
-//     //   console.log(e.target);
-//     // };
-//     const documentElements: any =
-//       window.document.body.querySelectorAll("Section");
-//     const documentElementsArray = [...documentElements];
-
-//     const getPosition = (element: any) => {
-//       var rect = element.getBoundingClientRect();
-//       return {
-//         x: rect.left,
-//         y: rect.top,
-//       };
-//     };
-//     documentElementsArray.map((element, index) => {
-//       //get position of element
-//       const { x, y } = getPosition(element);
-//       const elementX = Math.round((12 * x) / width);
-//       //get width of element
-//       const elementWidth = Math.round((12 * element.offsetWidth) / width);
-//       const elementheight = Math.round((12 * element.offsetHeight) / height);
-//       console.log("from key");
-//       const section = {
-//         i: index,
-//         text: element.innerHTML,
-//         w: elementWidth,
-//         h: elementheight,
-//         x: elementX,
-//         y: 0,
-//       };
-//       setAllElements((preState) => ({
-//         data: [...preState.data, { section }],
-//       }));
-//     });
-//     console.log(allElements);
-//   }
-// }, []);
